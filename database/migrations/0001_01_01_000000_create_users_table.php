071176eb66316c4cf56355fbc4bb16ba6bd23e63
Minh Vương <tmvuong2k4@gmail.com>
2025-03-06 15:01:49 +0700

    login page and app layout

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('Vaitro', 100)->default('sinhvien');
+            $table->enum('role', ['giangvien', 'sinhvien', 'quantri'])->default('sinhvien');
             $table->rememberToken();
             $table->timestamps();
         });
